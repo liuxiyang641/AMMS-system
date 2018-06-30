cc36e031eabd898570b12144010ac93d5fce86cf
wm <2751315301@qq.com>
2018-06-30 22:41:11 +0800

    nav+list

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <b-navbar toggleable="md" type="dark" variant="info">
+      <b-navbar-brand href="#">学术会议管理平台</b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav class="ml-auto">
+            <b-button style="margin-right: 4px;">登陆/ID</b-button>
+            <b-button>注册/登出</b-button>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <manege></manege>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import manege from '@/components/manege'
+  export default {
+    name: 'App',
+    components:{manege}
+  }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
